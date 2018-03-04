bbd82ce997fdce78ac0b648daae597891b44d1b2
gutow <gutow@uwosh.edu>
2018-03-03 19:53:55 -0600

    auto sizing iframes

---
+++ setiframeheight.js
@@ -0,0 +1,18 @@
+function getDocHeight(doc) {
+    doc = doc || document;
+    // stackoverflow.com/questions/1145850/
+    var body = doc.body, html = doc.documentElement;
+    var height = Math.max( body.scrollHeight, body.offsetHeight, 
+        html.clientHeight, html.scrollHeight, html.offsetHeight );
+    return height;
+}
+function setIframeHeighttoContentHeight(id) {
+    var ifrm = document.getElementById(id);
+    var doc = ifrm.contentDocument? ifrm.contentDocument: 
+        ifrm.contentWindow.document;
+    ifrm.style.visibility = 'hidden';
+    ifrm.style.height = "10px"; // reset to minimal height ...
+    // IE opt. for bing/msn needs a bit added or scrollbar appears
+    ifrm.style.height = getDocHeight( doc ) + 20 + "px";
+    ifrm.style.visibility = 'visible';
+}
